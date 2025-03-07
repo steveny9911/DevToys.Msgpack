45a031dc4b4aaaeb17adc448ee91302c2bf96d4e
steveny9911 <stevenywy@gmail.com>
2025-03-06 22:48:32 -0800

    hex pack

--- DevToys.Msgpack/MsgpackBase64JsonConverterGuiTool.cs
+++ DevToys.Msgpack/MsgpackBase64JsonConverterGuiTool.cs
@@ -55,8 +55,8 @@ internal sealed class MsgpackBase64JsonConverterGuiTool : IGuiTool, IDisposable
     private readonly DisposableSemaphore _semaphore = new();
     private readonly ILogger _logger;
     private readonly ISettingsProvider _settingsProvider;
-    private readonly IUIMultiLineTextInput _inputTextArea = MultiLineTextInput("msgpack-b64-json-input-text-area");
-    private readonly IUIMultiLineTextInput _outputTextArea = MultiLineTextInput("msgpack-b64-json-output-text-area");
+    private readonly IUIMultiLineTextInput _inputTextArea = MultiLineTextInput("msgpack-json-input-text-area");
+    private readonly IUIMultiLineTextInput _outputTextArea = MultiLineTextInput("msgpack-json-output-text-area");
 
     private CancellationTokenSource? _cancellationTokenSource;
     internal Task? WorkTask { get; private set; }
@@ -97,34 +97,44 @@ internal sealed class MsgpackBase64JsonConverterGuiTool : IGuiTool, IDisposable
                         Stack().Vertical().WithChildren(
                             Label()
                                 .Text(DevToysMsgpackResources.Configuration),
-                            Setting("msgpack-b64-json-text-conversion-setting")
+                            SettingGroup("msgpack-json-conversion-setting")
                                 .Icon("FluentSystemIcons", '\uF18D')
                                 .Title(DevToysMsgpackResources.ConversionTitle)
-                                .Description(DevToysMsgpackResources.ConversionDescription)
                                 .Handle(
                                     _settingsProvider,
                                     ConversionMode,
                                     OnConversionModeChanged,
-                                    Item(DevToysMsgpackResources.MsgpackBase64ToJson,
-                                        MsgpackJsonConversion.MsgpackBase64ToJson),
-                                    Item(DevToysMsgpackResources.JsonToMsgpackBase64,
-                                        MsgpackJsonConversion.JsonToMsgpackBase64),
-                                    Item(DevToysMsgpackResources.MsgpackHexToJson,
-                                        MsgpackJsonConversion.MsgpackHexToJson),
-                                    Item(DevToysMsgpackResources.JsonToMsgpackHex,
-                                        MsgpackJsonConversion.JsonToMsgpackHex)
-                                ),
-                            Setting("msgpack-b64-json-text-indentation-setting")
-                                .Icon("FluentSystemIcons", '\uF6F8')
-                                .Title(DevToysMsgpackResources.Indentation)
-                                .Handle(
-                                    _settingsProvider,
-                                    IndentationMode,
-                                    OnIndentationModelChanged,
-                                    Item(DevToysMsgpackResources.TwoSpaces, Indentation.TwoSpaces),
-                                    Item(DevToysMsgpackResources.FourSpaces, Indentation.FourSpaces),
-                                    Item(DevToysMsgpackResources.OneTab, Indentation.OneTab),
-                                    Item(DevToysMsgpackResources.Minified, Indentation.Minified)
+                                    Item(DevToysMsgpackResources.MsgpackBase64ToJson, MsgpackJsonConversion.MsgpackBase64ToJson),
+                                    Item(DevToysMsgpackResources.JsonToMsgpackBase64, MsgpackJsonConversion.JsonToMsgpackBase64),
+                                    Item(DevToysMsgpackResources.MsgpackHexToJson, MsgpackJsonConversion.MsgpackHexToJson),
+                                    Item(DevToysMsgpackResources.JsonToMsgpackHex, MsgpackJsonConversion.JsonToMsgpackHex)
+                                )
+                                .WithSettings(
+                                    Setting("msgpack-b64-json-text-indentation-setting")
+                                        .Icon("FluentSystemIcons", '\uF6F8')
+                                        .Title(DevToysMsgpackResources.IndentationTitle)
+                                        .Description(DevToysMsgpackResources.IndentationDescription)
+                                        .Handle(
+                                            _settingsProvider,
+                                            IndentationMode,
+                                            OnIndentationModelChanged,
+                                            Item(DevToysMsgpackResources.TwoSpaces, Indentation.TwoSpaces),
+                                            Item(DevToysMsgpackResources.FourSpaces, Indentation.FourSpaces),
+                                            Item(DevToysMsgpackResources.OneTab, Indentation.OneTab),
+                                            Item(DevToysMsgpackResources.Minified, Indentation.Minified)
+                                        ),
+                                    Setting("msgpack-hex-json-separator-setting")
+                                        .Icon("FluentSystemIcons", '\uE1A8')
+                                        .Title(DevToysMsgpackResources.SeparatorTitle)
+                                        .Description(DevToysMsgpackResources.SeparatorDescription)
+                                        .Handle(_settingsProvider,
+                                            SeparatorMode,
+                                            OnSeparatorModeChanged,
+                                            Item(DevToysMsgpackResources.None, Separator.None),
+                                            Item(DevToysMsgpackResources.Space, Separator.Space),
+                                            Item(DevToysMsgpackResources.Dash, Separator.Dash),
+                                            Item(DevToysMsgpackResources.Comma, Separator.Comma)
+                                        )
                                 )
                         )
                     ),
@@ -147,6 +157,7 @@ internal sealed class MsgpackBase64JsonConverterGuiTool : IGuiTool, IDisposable
                     )
                 )
         );
+
 
     // Smart detection handler.
     public void OnDataReceived(string dataTypeName, object? parsedData)
@@ -187,6 +198,11 @@ internal sealed class MsgpackBase64JsonConverterGuiTool : IGuiTool, IDisposable
         _inputTextArea.Text(_outputTextArea.Text);
     }
 
+    private void OnSeparatorModeChanged(Separator separatorMode)
+    {
+        StartConvert(_inputTextArea.Text);
+    }
+
     private void OnIndentationModelChanged(Indentation indentationMode)
     {
         StartConvert(_inputTextArea.Text);
@@ -224,89 +240,101 @@ internal sealed class MsgpackBase64JsonConverterGuiTool : IGuiTool, IDisposable
         switch (_settingsProvider.GetSetting(ConversionMode))
         {
             case MsgpackJsonConversion.JsonToMsgpackBase64:
-            {
-                byte[] bytes = MessagePackSerializer.ConvertFromJson(input, null, ct);
-                return Base64Helper.FromBytesToBase64(bytes, DefaultEncoding, _logger, ct);
-            }
+                return JsonToMsgpackBase64(input, ct);
             case MsgpackJsonConversion.MsgpackBase64ToJson:
-            {
-                if (string.IsNullOrWhiteSpace(input) || string.IsNullOrEmpty(input))
-                {
-                    return string.Empty;
-                }
-
-                byte[]? bytes = Base64Helper.FromBase64ToBytes(input, DefaultEncoding, _logger, ct);
-                if (bytes == null)
-                {
-                    return DevToysMsgpackResources.InvalidBase64;
-                }
-
-                string? rawJson;
-                try
-                {
-                    rawJson = MessagePackSerializer.ConvertToJson(bytes, null, ct);
-                }
-                catch (MessagePackSerializationException)
-                {
-                    return DevToysMsgpackResources.InvalidJson;
-                }
-
-                string? formattedJson = await JsonHelpers.FormatAsync(rawJson,
-                    _settingsProvider.GetSetting(IndentationMode), _logger, ct);
-                return formattedJson ?? rawJson;
-            }
+                return await MsgpackBase64ToJson(input, ct);
             case MsgpackJsonConversion.MsgpackHexToJson:
-            {
-                try
-                {
-                    input = HexHelpers.CleanHexRegex().Replace(input, "");
-                    byte[] bytes = Convert.FromHexString(input);
-                    string? rawJson;
-                    try
-                    {
-                        rawJson = MessagePackSerializer.ConvertToJson(bytes, null, ct);
-                    }
-                    catch (MessagePackSerializationException)
-                    {
-                        return DevToysMsgpackResources.InvalidJson;
-                    }
-
-                    string? formattedJson = await JsonHelpers.FormatAsync(rawJson,
-                        _settingsProvider.GetSetting(IndentationMode), _logger, ct);
-                    return formattedJson ?? rawJson;
-                }
-                catch (FormatException)
-                {
-                    return DevToysMsgpackResources.InvalidHex;
-                }
-                catch (MessagePackSerializationException)
-                {
-                    return DevToysMsgpackResources.InvalidJson;
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, "Failed to convert HEX to JSON.");
-                    return DevToysMsgpackResources.InvalidException;
-                }
-            }
+                return await MsgpackHexToJson(input, ct);
             case MsgpackJsonConversion.JsonToMsgpackHex:
+                return JsonToMsgpackHex(input, ct);
+            default:
+                throw new NotSupportedException();
+        }
+    }
+
+    private string JsonToMsgpackBase64(string input, CancellationToken ct)
+    {
+        byte[] bytes = MessagePackSerializer.ConvertFromJson(input, null, ct);
+        return Base64Helper.FromBytesToBase64(bytes, DefaultEncoding, _logger, ct);
+    }
+
+    private async Task<string> MsgpackBase64ToJson(string input, CancellationToken ct)
+    {
+        if (string.IsNullOrWhiteSpace(input) || string.IsNullOrEmpty(input))
+        {
+            return string.Empty;
+        }
+
+        byte[]? bytes = Base64Helper.FromBase64ToBytes(input, DefaultEncoding, _logger, ct);
+        if (bytes == null)
+        {
+            return DevToysMsgpackResources.InvalidBase64;
+        }
+
+        string? rawJson;
+        try
+        {
+            rawJson = MessagePackSerializer.ConvertToJson(bytes, null, ct);
+        }
+        catch (MessagePackSerializationException)
+        {
+            return DevToysMsgpackResources.InvalidJson;
+        }
+
+        string? formattedJson = await JsonHelpers.FormatAsync(rawJson,
+            _settingsProvider.GetSetting(IndentationMode), _logger, ct);
+        return formattedJson ?? rawJson;
+    }
+
+    private async Task<string> MsgpackHexToJson(string input, CancellationToken ct)
+    {
+        try
+        {
+            input = HexHelpers.CleanHexRegex().Replace(input, "");
+            byte[] bytes = Convert.FromHexString(input);
+            string? rawJson;
+            try
             {
-                byte[] bytes = MessagePackSerializer.ConvertFromJson(input, null, ct);
-                string hex = BitConverter.ToString(bytes);
-                switch (_settingsProvider.GetSetting(SeparatorMode))
-                {
-                    case Separator.None:
-                        return hex.Replace("-", "");
-                    case Separator.Space:
-                        return hex.Replace("-", " ");
-                    case Separator.Dash:
-                        return hex;
-                    case Separator.Comma:
-                        return hex.Replace("-", ",");
-                    default:
-                        throw new NotSupportedException();
-                }
+                rawJson = MessagePackSerializer.ConvertToJson(bytes, null, ct);
             }
+            catch (MessagePackSerializationException)
+            {
+                return DevToysMsgpackResources.InvalidJson;
+            }
+
+            string? formattedJson = await JsonHelpers.FormatAsync(rawJson,
+                _settingsProvider.GetSetting(IndentationMode), _logger, ct);
+            return formattedJson ?? rawJson;
+        }
+        catch (FormatException)
+        {
+            return DevToysMsgpackResources.InvalidHex;
+        }
+        catch (MessagePackSerializationException)
+        {
+            return DevToysMsgpackResources.InvalidJson;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to convert HEX to JSON.");
+            return DevToysMsgpackResources.InvalidException;
+        }
+    }
+
+    private string JsonToMsgpackHex(string input, CancellationToken ct)
+    {
+        byte[] bytes = MessagePackSerializer.ConvertFromJson(input, null, ct);
+        string hex = BitConverter.ToString(bytes);
+        switch (_settingsProvider.GetSetting(SeparatorMode))
+        {
+            case Separator.None:
+                return hex.Replace("-", "");
+            case Separator.Space:
+                return hex.Replace("-", " ");
+            case Separator.Dash:
+                return hex;
+            case Separator.Comma:
+                return hex.Replace("-", ",");
             default:
                 throw new NotSupportedException();
         }
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # DevToys Msgpack
 
-An extension for encoding and decoding MessagePack (in Base64) to and from JSON.
+An extension for encoding and decoding MessagePack (in Base64 and HEX) to and from JSON.
 
 Published on NuGet: https://www.nuget.org/packages/DevToys.Msgpack/
 
 ![](https://raw.githubusercontent.com/steveny9911/DevToys.Msgpack/refs/heads/master/docs/screenshot_1.png)
+![](https://raw.githubusercontent.com/steveny9911/DevToys.Msgpack/refs/heads/master/docs/screenshot_2.png)
